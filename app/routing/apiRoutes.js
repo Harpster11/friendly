39ab3220c644bde81a74264df81cf297f1871d6f
John Harper <jharper1@tulane.edu>
2019-10-22 19:05:32 -0400

    HTML files and routes setup

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,30 @@
+// create storage array for survey
+
+var surveyData = [
+    {
+        profileName: "Reba McIntire",
+        profilePhoto: "../data/reba.jpg",
+        scores:[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    }
+        ]
+
+
+//  export array to make it available throughout the app
+module.exports = surveyData;
+
+    
+    
+
+
+    
