673fdbd9192f9e4495a9e1b004a6f1770719c765
John Harper <jharper1@tulane.edu>
2019-10-24 18:59:05 -0400

    HTML pages finished, routes connected to page

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friends = [
+    {
+        name:"Hellen Keller",
+        photo:"",
+        scores: [
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5"
+        ]
+    }
+]
+
+module.exports = friends;
